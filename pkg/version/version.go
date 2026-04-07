52ba75bba08698616511a209f3bf2ec48a47337c
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-07 03:33:56 +0200

    feat: Add ZC1275 — use Zsh ${var:h} instead of dirname (#302)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 271 Katas = 0.2.71
-const Version = "0.2.71"
+// 272 Katas = 0.2.72
+const Version = "0.2.72"
