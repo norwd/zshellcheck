bcba52b55628426e1d685b02e3823d613a2936da
afadesigns <afadesign.official@gmail.com>
2026-04-13 22:32:09 +0200

    feat: Add ZC1302 — avoid help builtin in Zsh, use run-help or man

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 298 Katas = 0.2.98
-const Version = "0.2.98"
+// 299 Katas = 0.2.99
+const Version = "0.2.99"
