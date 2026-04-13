cb469c712abd21040ce34c71c02994c02e669f0c
afadesigns <afadesign.official@gmail.com>
2026-04-13 22:15:13 +0200

    feat: Add ZC1296 — avoid shopt in Zsh, use setopt/unsetopt instead

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 292 Katas = 0.2.92
-const Version = "0.2.92"
+// 293 Katas = 0.2.93
+const Version = "0.2.93"
