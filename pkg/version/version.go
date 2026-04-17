72e881687e185be1ce9e6b25c57ebc4b0672c251
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-17 08:49:07 +0200

    feat: ZC1414 — warn on `hash -d` (opposite semantics Bash vs Zsh) (#429)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 410 Katas = 0.4.10
-const Version = "0.4.10"
+// 411 Katas = 0.4.11
+const Version = "0.4.11"
