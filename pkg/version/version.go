90de4959a5276a9698aa41e3ae7a50769d45d2c1
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-17 02:26:35 +0200

    feat: ZC1348 — use Zsh glob type qualifiers instead of `find -type` (#361)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 344 Katas = 0.3.44
-const Version = "0.3.44"
+// 345 Katas = 0.3.45
+const Version = "0.3.45"
