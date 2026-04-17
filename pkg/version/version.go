8208e54bab26745f197ce7a41b4a6c06f2689fab
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-18 00:46:34 +0200

    feat: ZC1598 — flag `chmod` world-write on sensitive `/dev/` nodes (#617)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 594 Katas = 0.5.94
-const Version = "0.5.94"
+// 595 Katas = 0.5.95
+const Version = "0.5.95"
