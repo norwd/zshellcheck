a17c6e7844a1b86f806963a1e2f519dca7e54903
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-17 04:33:39 +0200

    feat: ZC1371 — use Zsh array `:t` modifier instead of `basename -a` (#384)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 367 Katas = 0.3.67
-const Version = "0.3.67"
+// 368 Katas = 0.3.68
+const Version = "0.3.68"
