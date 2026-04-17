74e032a108e41e8280385a1b5a4daa281cf923e4
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-18 01:48:31 +0200

    feat: ZC1611 — prefer Zsh `${(U)var}` / `${(L)var}` over Bash `${var^^}` / `${var,,}` (#630)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 607 Katas = 0.6.7
-const Version = "0.6.7"
+// 608 Katas = 0.6.8
+const Version = "0.6.8"
