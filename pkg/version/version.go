64fc106824be01888a60b5bd0cf546a18830cec2
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-18 00:50:29 +0200

    feat: ZC1599 — flag `ldconfig -f PATH` with non-`/etc/` loader config (#618)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 595 Katas = 0.5.95
-const Version = "0.5.95"
+// 596 Katas = 0.5.96
+const Version = "0.5.96"
