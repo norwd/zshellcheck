daa8799f259b78e4b3ea78904c004f4e97f26559
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-17 07:23:24 +0200

    feat: ZC1400 — use Zsh $CPUTYPE instead of parsing $HOSTTYPE (#414)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 396 Katas = 0.3.96
-const Version = "0.3.96"
+// 397 Katas = 0.3.97
+const Version = "0.3.97"
