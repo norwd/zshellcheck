1e860064051192d6e4e9459eb023f1d42799b533
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-17 02:10:02 +0200

    feat: ZC1344 — use Zsh `*(L±Nk)` glob qualifier instead of `find -size` (#357)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 340 Katas = 0.3.40
-const Version = "0.3.40"
+// 341 Katas = 0.3.41
+const Version = "0.3.41"
