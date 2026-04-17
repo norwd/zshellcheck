df8a7e7559358e9278ed81c7deb282ec13f724a1
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-17 08:01:59 +0200

    feat: ZC1406 — prefer Zsh zargs -P over xargs -P for parallel execution (#420)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 402 Katas = 0.4.2
-const Version = "0.4.2"
+// 403 Katas = 0.4.3
+const Version = "0.4.3"
