9269159889bd82a7fa34cc3c557cceb8d314ddd5
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-17 02:21:23 +0200

    feat: ZC1347 — use Zsh `*(g:name:)` glob qualifier instead of `find -group` (#360)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 343 Katas = 0.3.43
-const Version = "0.3.43"
+// 344 Katas = 0.3.44
+const Version = "0.3.44"
