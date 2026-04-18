d6484f5845727c76b74538bac29ca1c1070b2f0b
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-18 22:17:51 +0200

    feat: ZC1716 — flag `uname -m` / `-p` (use $CPUTYPE / $MACHTYPE) (#736)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 712 Katas = 0.7.12
-const Version = "0.7.12"
+// 713 Katas = 0.7.13
+const Version = "0.7.13"
