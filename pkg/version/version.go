423931a83ca6443ecc4ccf917da8738645c3b533
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-18 21:05:49 +0200

    feat: ZC1710 — flag journalctl --vacuum-size=1 / --vacuum-time=1s journal wipe (#730)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 706 Katas = 0.7.6
-const Version = "0.7.6"
+// 707 Katas = 0.7.7
+const Version = "0.7.7"
