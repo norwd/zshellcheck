afebe045cf4d57f0fffa2930983497fb929fd4ef
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-19 00:56:06 +0200

    feat: ZC1737 — flag `wpa_passphrase SSID PASSWORD` (Wi-Fi pass in argv) (#757)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 733 Katas = 0.7.33
-const Version = "0.7.33"
+// 734 Katas = 0.7.34
+const Version = "0.7.34"
