66ffff75ee33fa67417dd4f063cf2c40e23f9693
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-18 08:07:19 +0200

    feat: ZC1680 — flag ansible-playbook vault-password-file in world-traversable path (#700)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 676 Katas = 0.6.76
-const Version = "0.6.76"
+// 677 Katas = 0.6.77
+const Version = "0.6.77"
