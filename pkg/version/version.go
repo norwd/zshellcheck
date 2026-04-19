475c224ae3aac25765000a7364e6b5a42666328a
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-20 00:20:20 +0200

    feat: ZC1929 — detect `cpio -i` without `--no-absolute-filenames` path escape (#950)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 925 Katas = 0.9.25
-const Version = "0.9.25"
+// 926 Katas = 0.9.26
+const Version = "0.9.26"
