10f37af34a2dcd5866ed91922f8ebc8c35835561
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-19 18:47:22 +0200

    feat: ZC1873 — warn on `setopt ERR_RETURN` bailing functions on first non-zero exit (#894)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 869 Katas = 0.8.69
-const Version = "0.8.69"
+// 870 Katas = 0.8.70
+const Version = "0.8.70"
