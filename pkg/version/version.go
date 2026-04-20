543d891e394ea6732b9f09ba69d6c0330decff3c
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-20 03:22:38 +0200

    feat: ZC1965 — detect `systemd-cryptenroll --wipe-slot=all` LUKS keyslot wipe (#986)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 961 Katas = 0.9.61
-const Version = "0.9.61"
+// 962 Katas = 0.9.62
+const Version = "0.9.62"
